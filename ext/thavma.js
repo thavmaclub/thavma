aa981c7935835eb0f8442c718d8ab682adebb81a
THAVMA <team@thavma.club>
2022-01-30 10:25:42 -0800

    feat(ext/thavma): add console warnings

--- ext/thavma.js
+++ ext/thavma.js
@@ -1,10 +1,14 @@
 console.log('Installing thavma extension script...');
 window.addEventListener('message', async (evt) => {
   console.log('Received message:', evt);
-  if (typeof evt.data !== 'object') return;
-  if (Number.isNaN(Number(evt.data.id))) return;
-  if (typeof evt.data.pwd !== 'string') return;
-  if (evt.data.pwd.length !== 21) return;
+  if (typeof evt.data !== 'object')
+    return console.warn('Message data not an object');
+  if (Number.isNaN(Number(evt.data.id)))
+    return console.warn('Message ID not a number');
+  if (typeof evt.data.pwd !== 'string')
+    return console.warn('Message PWD not a string');
+  if (evt.data.pwd.length !== 21)
+    return console.warn('Message PWD not 21 chars');
   console.log('Setting assessment...', evt.data);
   await browser.storage.local.set({ id: evt.data.id, pwd: evt.data.pwd });
   window.postMessage('THAVMA_EXT_ACTIVE');
