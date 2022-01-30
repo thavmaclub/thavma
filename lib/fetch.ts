2c1e386af5f5ad8af7f8d87732e0b4cb8ee6b272
THAVMA <team@thavma.club>
2022-01-29 21:05:30 -0800

    feat(assessments/id): use SWR to fetch with pwd

---
+++ lib/fetch.ts
@@ -0,0 +1,28 @@
+import { mutate } from 'swr';
+import to from 'await-to-js';
+
+import { APIError } from 'lib/model';
+
+export async function fetcher<T, D = T>(
+  url: string,
+  method: 'get' | 'put' | 'post' | 'patch' | 'delete' = 'get',
+  data?: D
+): Promise<T> {
+  const body = data ? JSON.stringify(data) : undefined;
+  const headers = data ? { 'Content-Type': 'application/json' } : undefined;
+  const [err, res] = await to<Response>(fetch(url, { headers, method, body }));
+  if (res && !res.ok) {
+    const { message } = (await res.json()) as APIError;
+    const msg = `API (${url}) responded with error: ${message}`;
+    throw new APIError(msg, res.status);
+  } else if (err) {
+    throw new APIError(`${err.name} calling API (${url}): ${err.message}`);
+  } else if (!res) {
+    throw new APIError(`No response from API (${url})`);
+  }
+  return res.json() as Promise<T>;
+}
+
+export async function prefetch(url: string): Promise<void> {
+  if (url) await mutate(url, fetcher(url));
+}
--- pages/assessments/[id].tsx
+++ pages/assessments/[id].tsx
@@ -1,55 +1,39 @@
-import { useEffect, useState } from 'react';
 import cn from 'classnames';
 import rfdc from 'rfdc';
+import { useMemo } from 'react';
 import { useRouter } from 'next/router';
+import useSWR from 'swr';
 
 import Header from 'components/header';
 import Page from 'components/page';
 
 import { Assessment } from 'lib/model';
-import supabase from 'lib/supabase';
+import { fetcher } from 'lib/fetch';
 
 const clone = rfdc();
 
 export default function AssessmentPage(): JSX.Element {
   const { query } = useRouter();
-  const [assessment, setAssessment] = useState<Assessment>();
-  useEffect(() => {
-    void (async () => {
-      if (typeof query.id !== 'string') return console.warn('No query ID.');
-      const { data } = await supabase
-        .from<Assessment>('assessments')
-        .select()
-        .eq('id', query.id);
-      setAssessment((prev) => data ? data[0] : prev);
-    })();
-  }, [query.id]);
-  useEffect(() => {
-    async function update(): Promise<void> {
-      if (!assessment) return console.warn('No assessment to update.');
-      const { data, error } = await supabase
-        .from<Assessment>('assessments')
-        .update({ questions: assessment.questions })
-        .eq('id', assessment.id);
-      if (error) console.error(error.message);
-      console.log('Updated assessment:', data);
-    }
-    const timeoutId = setTimeout(() => {
-      void update();
-    }, 500);
-    return () => clearTimeout(timeoutId);
-  }, [assessment]);
+  const url = useMemo(
+    () =>
+      typeof query.id === 'string' && typeof query.pwd === 'string'
+        ? `/api/assessments/${query.id}?pwd=${query.pwd}`
+        : null,
+    [query.id, query.pwd]
+  );
+  const { data, mutate } = useSWR<Assessment>(url, fetcher);
   return (
     <Page name='Assessment'>
       <main>
         <Header />
         <article className='wrapper'>
-          <h2 className={cn({ loading: !assessment })}>
-            {assessment?.name}
-          </h2>
-          {assessment?.questions.map((question, questionIdx) => (
+          <h2 className={cn({ loading: !data })}>{data?.name}</h2>
+          {data?.questions.map((question, questionIdx) => (
             <section key={questionIdx}>
-              <p><b>{questionIdx + 1}. </b>{question.question}</p>
+              <p>
+                <b>{questionIdx + 1}. </b>
+                {question.question}
+              </p>
               <ul>
                 {question.answers.map((answer, answerIdx) => (
                   <li key={answerIdx}>
@@ -59,18 +43,16 @@ export default function AssessmentPage(): JSX.Element {
                       value={answerIdx}
                       className='radio'
                       checked={question.answer === answerIdx}
-                      onChange={() => setAssessment((prev) => {
-                        if (!prev) return prev;
-                        console.log('Prev:', prev);
-                        const updated = clone(prev);
-                        console.log('Updated:', updated);
+                      onChange={async () => {
+                        const updated = clone(data);
                         updated.questions[questionIdx].answer = answerIdx;
-                        console.log('Updated question:', updated.questions[questionIdx]);
-                        console.log('Updated:', updated);
-                        return updated;
-                      })}
+                        await mutate(updated, false);
+                        if (url) await mutate(fetcher(url, 'patch', updated));
+                      }}
                     />
-                    <label htmlFor={`${questionIdx}-${answerIdx}`}>{answer}</label>
+                    <label htmlFor={`${questionIdx}-${answerIdx}`}>
+                      {answer}
+                    </label>
                   </li>
                 ))}
               </ul>
@@ -86,12 +68,12 @@ export default function AssessmentPage(): JSX.Element {
             margin: 0;
             padding: 0;
           }
-          
+
           li {
             list-style: none;
             margin: 4px 0;
           }
-          
+
           label {
             margin-left: 8px;
           }
