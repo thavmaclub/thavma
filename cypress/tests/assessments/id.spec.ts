8e58335cb74177f27209cb93430d524444ff6b8c
THAVMA <team@thavma.club>
2022-01-30 19:19:46 -0800

    test(assessments/id): add integration tests

---
+++ cypress/tests/assessments/id.spec.ts
@@ -0,0 +1,89 @@
+import assessment from 'cypress/fixtures/assessment.json';
+
+describe('Assessment PG', () => {
+  it('shows waiting empty placeholder', () => {
+    cy.seed({ skipQuestions: true }).then((ids) => {
+      const url = `/assessments/${ids.assessment}?pwd=${assessment.pwd}`;
+      cy.intercept('GET', `/api${url}`).as('get-assessment');
+      cy.visit(url);
+    });
+    cy.wait('@get-assessment').its('response.statusCode').should('eq', 200);
+    cy.get('h2').should('contain', assessment.name);
+    cy.getBySel('question').should('not.exist');
+    cy.contains('waiting for questions...').should('be.visible');
+    cy.percySnapshot('Assessment Waiting');
+  });
+
+  it('answers assessment questions', () => {
+    cy.seed().then((ids) => {
+      const url = `/assessments/${ids.assessment}?pwd=${assessment.pwd}`;
+      cy.intercept('GET', `/api${url}`).as('get-assessment');
+      cy.intercept('PATCH', `/api${url}`).as('update-assessment');
+      cy.visit(url);
+    });
+    cy.wait('@get-assessment').its('response.statusCode').should('eq', 200);
+    cy.get('h2').should('contain', assessment.name);
+    cy.getBySel('question').should('have.length', assessment.questions.length);
+    // Asserting about all 50 questions causes Cypress to stall so I'm only
+    // checking the first 5 questions and then assuming the rest are OK.
+    assessment.questions.slice(0, 5).forEach((question, questionIdx) => {
+      cy.getBySel('question')
+        .eq(questionIdx)
+        .within(() => {
+          cy.get('b').should('have.text', `${questionIdx + 1}. `);
+          cy.get('p').should('contain', question.question);
+          cy.get('li').should('have.length', question.answers.length);
+          question.answers.forEach((answer, answerIdx) => {
+            cy.get('li')
+              .eq(answerIdx)
+              .within(() => {
+                cy.get('input')
+                  .should(
+                    question.answer === answerIdx
+                      ? 'be.checked'
+                      : 'not.be.checked'
+                  )
+                  .and('have.attr', 'type', 'radio');
+                cy.get('label').should('have.text', answer);
+              });
+          });
+        });
+    });
+    cy.getBySel('question')
+      .first()
+      .within(() => {
+        cy.get('input:not(:checked)')
+          .first()
+          .should('not.be.checked')
+          .click()
+          .should('be.checked');
+        cy.wait('@update-assessment')
+          .its('response.statusCode')
+          .should('eq', 200);
+      });
+    cy.percySnapshot('Assessment');
+  });
+
+  it('requires assessment password', () => {
+    cy.seed().then((ids) => {
+      cy.visit(`/assessments/${ids.assessment}`);
+      cy.contains('unauthorized - missing assessment pwd').should('be.visible');
+      cy.get('h2').should('have.text', `assessment ${ids.assessment}`);
+      cy.percySnapshot('Assessment Unauthorized');
+    });
+  });
+
+  it('checks for valid password', () => {
+    cy.seed().then((ids) => {
+      const url = `/assessments/${ids.assessment}?pwd=l0R3m_1psUm`;
+      cy.intercept('GET', `/api${url}`).as('get-assessment');
+      cy.visit(url);
+      cy.wait('@get-assessment').its('response.statusCode').should('eq', 500);
+      cy.get('h2').should('have.text', `assessment ${ids.assessment}`);
+      cy.contains('error (500) fetching assessment')
+        .should('be.visible')
+        .and('contain', 'No data');
+      cy.percySnapshot('Assessment 500');
+    });
+  });
+});
