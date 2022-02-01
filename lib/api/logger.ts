7b480365b42eb53eec394a9e50fbdd802f6837e6
THAVMA <team@thavma.club>
2022-01-31 20:26:07 -0800

    feat(api/logger): replace `winston` with `loglevel`

--- lib/api/logger.ts
+++ lib/api/logger.ts
@@ -1,7 +1,1 @@
-import winston from 'winston';
-
-export default winston.createLogger({
-  level: 'verbose',
-  format: winston.format.simple(),
-  transports: [new winston.transports.Console()],
-});
+export { default } from 'loglevel';
