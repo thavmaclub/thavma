e8a5a96ec422d3620c5a8b1e54c7ac5dce60379c
THAVMA <team@thavma.club>
2022-01-31 20:27:28 -0800

    feat(lib/fetch): add token auth header

--- lib/fetch.ts
+++ lib/fetch.ts
@@ -1,15 +1,18 @@
-import { mutate } from 'swr';
 import to from 'await-to-js';
 
 import { APIError } from 'lib/model';
+import supabase from 'lib/supabase';
 
-export async function fetcher<T, D = T>(
+export default async function fetcher<T, D = T>(
   url: string,
   method: 'GET' | 'PUT' | 'POST' | 'PATCH' | 'DELETE' = 'GET',
   data?: D
 ): Promise<T> {
   const body = data ? JSON.stringify(data) : undefined;
-  const headers = data ? { 'Content-Type': 'application/json' } : undefined;
+  const token = supabase.auth.session()?.access_token;
+  const headers: Record<string, string> = {};
+  if (data) headers['Content-Type'] = 'application/json';
+  if (token) headers.Authorization = `Bearer ${token}`;
   const [err, res] = await to<Response>(fetch(url, { headers, method, body }));
   if (res && !res.ok) {
     const { message } = (await res.json()) as APIError;
@@ -21,8 +24,4 @@ export async function fetcher<T, D = T>(
     throw new APIError(`No response from API (${url})`);
   }
   return res.json() as Promise<T>;
-}
-
-export async function prefetch(url: string): Promise<void> {
-  if (url) await mutate(url, fetcher(url));
 }
